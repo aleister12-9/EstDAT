e318f4d4035e9c4619d012a38e806d0c12dd276b
aleister12-9 <izan.robles@estudiante.uam.es>
2025-02-07 18:04:45 +0100

    Update vertex.c

--- vertex.c
+++ vertex.c
@@ -1,60 +1,130 @@
 #include "vertex.h"
 #include <string.h>
 
-
 /*----------------------------------------------------------------------------------------*/
-/*
-Private function:
-*/
-Status vertex_setField (Vertex *v, char *key, char *value);
+Status vertex_setField(Vertex *v, char *key, char *value);
 
-Status vertex_setField (Vertex *v, char *key, char *value) {
-  if (!key || !value) return ERROR;
+Status vertex_setField(Vertex *v, char *key, char *value)
+{
+	if (!key || !value || !v)
+		return ERROR;
 
-  if (strcmp(key, "id") == 0) {
-    return vertex_setId(v, atol(value));
-  } else if (strcmp(key, "tag") == 0) {
-    return vertex_setTag(v, value);
-  } else if (strcmp(key, "state") == 0) {
-    return vertex_setState(v, (Label)atoi(value));
-  }
+	if (strcmp(key, "id") == 0)
+	{
+		return vertex_setId(v, atol(value));
+	}
+	else if (strcmp(key, "tag") == 0)
+	{
+		return vertex_setTag(v, value);
+	}
+	else if (strcmp(key, "state") == 0)
+	{
+		return vertex_setState(v, (Label)atoi(value));
+	}
 
-  return ERROR;
+	return ERROR;
 }
 
 /*----------------------------------------------------------------------------------------*/
-Vertex *vertex_initFromString(char *descr){
-  char buffer[1024];
-  char *token;
-  char *key;
-  char *value;
-  char *p;
-  Vertex *v;
+Vertex *vertex_initFromString(char *descr)
+{
+	char buffer[1024];
+	char *token;
+	char *key;
+	char *value;
+	char *p;
+	Vertex *v;
+	Status temp;
 
-  /* Check args: */
-  if (!descr) return NULL;
+	/* Check args: */
+	if (!descr)
+		return NULL;
 
-  /* Allocate memory for vertex: */
-  v = vertex_init();
-  if (!v) return NULL;
+	/* Allocate memory for vertex: */
+	v = vertex_init();
+	if (!v)
+		return NULL;
 
-  /* Read and tokenize description: */
-  sprintf(buffer, "%s", descr);
-  token = strtok(buffer, " \t\n");
-  while (token) {
-    p = strchr(token, ':');
-    if (!p) continue;
+	/* Read and tokenize description: */
+	sprintf(buffer, "%s", descr);
+	token = strtok(buffer, " \t\n");
+	while (token)
+	{
+		p = strchr(token, ':');
+		if (!p)
+			continue;
 
-    *p = '\0';
-    key = token;
-    value = p+1;
+		*p = '\0';
+		key = token;
+		value = p + 1;
 
-    vertex_setField(v, key, value);
+		temp = vertex_setField(v, key, value);
 
-    token = strtok(NULL, " \t\n");
-  }
+		if (temp == ERROR)
+		{
+			return NULL;
+		}
+		
 
-  return v;
+		token = strtok(NULL, " \t\n");
+	}
+
+	return v;
 }
 
-/**  rest of the functions in vertex.h **/
+/*----------------------------------------------------------------------------------------*/
+Vertex *vertex_init()
+{
+
+	Vertex *main = NULL;
+
+	if (!(main = (Vertex *)malloc(sizeof(Vertex))))
+	{
+		return NULL;
+	}
+
+	main->state = WHITE;
+	main->id = 0;
+	strcpy(main->tag, "\"\""); /*Revisar*/
+
+	return main;
+}
+
+/*----------------------------------------------------------------------------------------*/
+void vertex_free(void *v)
+{
+	if (v != NULL)
+	{
+		free(v);
+	}
+}
+
+/*----------------------------------------------------------------------------------------*/
+long vertex_getId(const Vertex *v)
+{
+	if (v == NULL)
+	{
+		return -1;
+	}
+	
+	return v->id;
+}
+
+/*----------------------------------------------------------------------------------------*/
+const char* vertex_getTag(const Vertex *v) 
+{
+    if (!v)
+	{
+		return NULL;
+	}
+	
+    return v->tag;  
+}
+
+/*----------------------------------------------------------------------------------------*/
+Label vertex_getState (const Vertex * v)
+{
+	return v->state;
+}
+
+
