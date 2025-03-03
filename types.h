4319a687bbcec634702271535c0b2b6df960f70e
Aperezn2006 <62878714+Aperezn2006@users.noreply.github.com>
2025-03-03 10:43:48 +0100

    Add files via upload

--- types.h
+++ types.h
@@ -1,32 +1,18 @@
-/**
- * @file types.h
- * @author Profesores Programación 2
- * @date 2 February 2020
- * @brief ADT Boolean and Status
- *
- * @details Here typically goes a more extensive explanation of what the header
- * defines. Doxygens tags are words preceeded by @.
- * 
- * @see 
+/* 
+ * File:   types.h
+ * Author: Profesores de PROG2
  */
 
-#ifndef TYPES_H_
-#define TYPES_H_
+#ifndef TYPES_H
+#define	TYPES_H
 
-/**
- * @brief ADT Boolean
- */
 typedef enum {
-    FALSE=0, /*!< False value */
-    TRUE=1  /*!< True value  */
+    FALSE=0, TRUE=1
 } Bool;
 
-/** 
- * @brief ADT Status
- */
 typedef enum {
-    ERROR=0, /*!< To codify an ERROR output */
-    OK=1     /*!< OK output */
+    ERROR=0, OK=1, END=2
 } Status;
 
-#endif /* TYPES_H_ */
+#endif	/* TYPES_H */
+
